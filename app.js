697839654f2ffbf37f21db0d3886fa99558f85cb
Jose Luis Checa Exposito <joselchecaexpo@gmail.com>
2017-03-13 20:52:44 +0000

    simple login

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const server = express();
 
 const port = process.env.port || 3000;
 
-server.get('/', (req, res, err) => {
-	res.send('SCRATMATCH');
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.static('public'));
+
+server.get('/', (req, res) => {
+	res.sendFile('index.html');
 });
+server.post('/login', (req,res,next) => {
+    if(req.body.name === 'scratch' && req.body.password === '000000'){
+        console.log(req.body);
+        res.send(JSON.stringify({success: true,data: req.body}));
+    } else {
+        res.send(JSON.stringify({success: false}))
+    }
+
+})
 
 server.listen(port, (err) => {
 	if(err){
